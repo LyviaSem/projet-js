bd866fb8c99c0e90c4416e234572127328894022
abdelrazak27 <a271101m090100@gmail.com>
2022-11-23 19:12:22 +0100

    filter par, ok

--- js/main.js
+++ js/main.js
@@ -1,101 +1,98 @@
-function afficherNftParPages() {
-    for (let j = 1; j <= 5; j++) {
-        const titreNumeroPage = document.createElement('h1');
-        titreNumeroPage.classList.add('titre');
-        titreNumeroPage.innerHTML = "PAGE " + j
-        document.body.append(titreNumeroPage);
+let dataNFT = [];
 
-        const elementsPage = document.createElement('div');
-        elementsPage.classList.add('cards');
-        elementsPage.id = "elements" + j;
-        document.body.append(elementsPage);
+function createCard(NFT) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+        const cardTop = document.createElement('div')
+        cardTop.classList.add('card-top')
         
-        fetch('https://awesome-nft-app.herokuapp.com/?page=' + j)
+            const cardImage = document.createElement('img')
+            cardImage.classList.add('card-image')
+            cardImage.src = NFT.image_url
+            
+            const cardButtons = document.createElement('div')
+            cardButtons.classList.add('card-buttons')
+            
+                const buttonFav = document.createElement('button')
+                buttonFav.classList.add('card-fav')
+                buttonFav.innerHTML = "<i class='bx bx-star'></i>"
+                
+                const buttonPanier = document.createElement('button')
+                buttonPanier.classList.add('card-panier')
+                buttonPanier.innerHTML = "<i class='bx bx-cart'></i>"
+            
+        const cardInfos = document.createElement('div')
+        cardInfos.classList.add('card-infos')
+        
+            const cardName = document.createElement('p')
+            cardName.classList.add('card-name')
+            cardName.innerHTML = NFT.name;
+            
+            const cardCreator = document.createElement('p')
+            cardCreator.classList.add('card-creator')
+            cardCreator.innerHTML = "<i class='bx bxs-user' style='color:#FF7F50'></i> <span>Créateur: </span>" + NFT.creator.username;
+
+            const cardSales = document.createElement('p')
+            cardSales.classList.add('card-sales')
+            cardSales.innerHTML = "<i class='bx bx-money-withdraw' style='color:#FF7F50'></i> <span>Nombre de ventes: </span>" + NFT.sales;
+
+            const cardCollection = document.createElement('p')
+            cardCollection.classList.add('card-collection')
+            cardCollection.innerHTML = "<i class='bx bxs-collection' style='color:#FF7F50'  ></i> <span>Collection: </span>" + NFT.collection.name;
+
+            const buttonInfos = document.createElement('button')
+            buttonInfos.classList.add('card-detail')
+            buttonInfos.innerHTML = "Plus d'informations <i class='bx bx-plus'></i>"
+            buttonInfos.setAttribute('onclick','toggleSide(this)');
+
+        const cardInfosDetails = document.createElement('div')
+        cardInfosDetails.classList.add('card-infos-details')
+
+            const cardNameDetail = document.createElement('p')
+            cardNameDetail.classList.add('card-name')
+            cardNameDetail.innerHTML = NFT.name;
+
+            const buttonInfosDetails = document.createElement('button')
+            buttonInfosDetails.classList.add('card-detail')
+            buttonInfosDetails.innerHTML = "Plus d'informations <i class='bx bx-plus'></i>"
+            buttonInfosDetails.setAttribute('onclick','toggleSide(this)');
+
+            const description = document.createElement('p')
+            description.classList.add('description')
+            description.innerHTML = NFT.description;
+            
+
+
+            document.getElementById('elements').append(card)
+                card.append(cardTop)
+                    if(NFT.image_url != "") cardTop.append(cardImage)
+                    cardTop.append(cardButtons)
+                        cardButtons.append(buttonFav)
+                        cardButtons.append(buttonPanier)
+                    cardInfos.append(cardName)
+                    cardInfos.append(cardCreator)
+                    cardInfos.append(cardSales)
+                    cardInfos.append(cardCollection)
+                    card.append(cardInfos)
+                    cardInfosDetails.append(cardNameDetail)
+                    cardInfosDetails.append(description)
+                    cardInfosDetails.append(buttonInfosDetails)
+                    card.append(cardInfosDetails)
+                    cardInfos.append(buttonInfos)
+    
+}
+
+async function afficherNftParPages() {
+    for (let j = 1; j <= 5; j++) {
+        
+        await fetch('https://awesome-nft-app.herokuapp.com/?page=' + j)
         .then(response => {
             if(response.ok) {
                 response.json().then(data => {
                     for (let i = 0; i < data.assets.length; i++) {
-    
-                        const card = document.createElement('div')
-                        card.classList.add('card')
-    
-                            const cardTop = document.createElement('div')
-                            cardTop.classList.add('card-top')
-                            
-                                const cardImage = document.createElement('img')
-                                cardImage.classList.add('card-image')
-                                cardImage.src = data.assets[i].image_url
-                                
-                                const cardButtons = document.createElement('div')
-                                cardButtons.classList.add('card-buttons')
-                                
-                                    const buttonFav = document.createElement('button')
-                                    buttonFav.classList.add('card-fav')
-                                    buttonFav.innerHTML = "<i class='bx bx-star'></i>"
-                                    
-                                    const buttonPanier = document.createElement('button')
-                                    buttonPanier.classList.add('card-panier')
-                                    buttonPanier.innerHTML = "<i class='bx bx-cart'></i>"
-                                
-                            const cardInfos = document.createElement('div')
-                            cardInfos.classList.add('card-infos')
-                            
-                                const cardName = document.createElement('p')
-                                cardName.classList.add('card-name')
-                                cardName.innerHTML = data.assets[i].name;
-                                
-                                const cardCreator = document.createElement('p')
-                                cardCreator.classList.add('card-creator')
-                                cardCreator.innerHTML = "<i class='bx bxs-user' style='color:#FF7F50'></i> <span>Créateur: </span>" + data.assets[i].creator.username;
-    
-                                const cardSales = document.createElement('p')
-                                cardSales.classList.add('card-sales')
-                                cardSales.innerHTML = "<i class='bx bx-money-withdraw' style='color:#FF7F50'></i> <span>Nombre de ventes: </span>" + data.assets[i].sales;
-    
-                                const cardCollection = document.createElement('p')
-                                cardCollection.classList.add('card-collection')
-                                cardCollection.innerHTML = "<i class='bx bxs-collection' style='color:#FF7F50'  ></i> <span>Collection: </span>" + data.assets[i].collection.name;
-    
-                                const buttonInfos = document.createElement('button')
-                                buttonInfos.classList.add('card-detail')
-                                buttonInfos.innerHTML = "Plus d'informations <i class='bx bx-plus'></i>"
-                                buttonInfos.setAttribute('onclick','toggleSide(this)');
-
-                            const cardInfosDetails = document.createElement('div')
-                            cardInfosDetails.classList.add('card-infos-details')
-
-                                const cardNameDetail = document.createElement('p')
-                                cardNameDetail.classList.add('card-name')
-                                cardNameDetail.innerHTML = data.assets[i].name;
-
-                                const buttonInfosDetails = document.createElement('button')
-                                buttonInfosDetails.classList.add('card-detail')
-                                buttonInfosDetails.innerHTML = "Plus d'informations <i class='bx bx-plus'></i>"
-                                buttonInfosDetails.setAttribute('onclick','toggleSide(this)');
-
-                                const description = document.createElement('p')
-                                description.classList.add('description')
-                                description.innerHTML = data.assets[i].description;
-                                
-                    
-            
-                                document.getElementById('elements' + j).append(card)
-                                    card.append(cardTop)
-                                        if(data.assets[i].image_url != "") cardTop.append(cardImage)
-                                        cardTop.append(cardButtons)
-                                            cardButtons.append(buttonFav)
-                                            cardButtons.append(buttonPanier)
-                                        cardInfos.append(cardName)
-                                        cardInfos.append(cardCreator)
-                                        cardInfos.append(cardSales)
-                                        cardInfos.append(cardCollection)
-                                        card.append(cardInfos)
-                                        cardInfosDetails.append(cardNameDetail)
-                                        cardInfosDetails.append(description)
-                                        cardInfosDetails.append(buttonInfosDetails)
-                                        card.append(cardInfosDetails)
-                                        cardInfos.append(buttonInfos)
-    
+                        data.assets[i].creator.username = data.assets[i].creator.username || " inconnu";
+                        dataNFT.push(data.assets[i]);
                     }
                 })
             } else {
@@ -103,6 +100,10 @@ function afficherNftParPages() {
                 document.getElementById('erreur').innerHTML = "Erreur"
             }
         })
+    }
+    
+    for (let k = 0; k < dataNFT.length; k++) {
+        createCard(dataNFT[k]);
     }
 }
 
@@ -122,10 +123,25 @@ function toggleSide(e) {
     }
 }
 
-// let selectFilter = document.getElementById('select-filter');
-// selectFilter.addEventListener('change', function() {
-//     console.log('You selected: ', this.value);
-// });
+let selectFilter = document.getElementById('select-filter');
+selectFilter.addEventListener('change', function() {
+
+    const allCards = document.getElementById('elements');
+    allCards.innerHTML = "";
+    let newDataNFT = [... dataNFT];
+
+    switch (this.value) {
+        case 'createurs':
+            newDataNFT.sort((a, b) => a.creator.username.localeCompare(b.creator.username)); 
+        break;
+        case 'ventes':
+            newDataNFT.sort((a, b) => b.sales - a.sales);
+        break;
+    }
+    for (let k = 0; k < newDataNFT.length; k++) {
+        createCard(newDataNFT[k]);
+    }
+});
 
 
 
